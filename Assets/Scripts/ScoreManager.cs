2a8e396c0935578407950f839157ff25e1f63512
Benedict Fritz <benedictfritz@gmail.com>
2013-01-06 15:55:08 -0600

    added score text that updates.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,15 +6,17 @@ public class ScoreManager : MonoBehaviour {
 	public static int player1Score = 0;
 	public static int player2Score = 0;
 	
+	public OTTextSprite player1ScoreText;
+	public OTTextSprite player2ScoreText;
+	
+	public void Update() {
+		player1ScoreText.text = player1Score.ToString();
+		player2ScoreText.text = player2Score.ToString();
+	}
+	
 	public static void IncreaseScore(string player) {
-		if (player.Equals("player1")) {
-			player1Score++;
-			Debug.Log("Player 1: " + player1Score);
-		}
-		else if (player.Equals("player2")) {
-			player2Score++;
-			Debug.Log("Player 2: " + player2Score);
-		}
+		if (player.Equals("player1")) { player1Score++; }
+		else if (player.Equals("player2")) { player2Score++; }
 	}
 }
 
