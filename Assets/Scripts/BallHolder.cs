3d4bde416255fbf7affb6e46e3cf3d6b8abd1c5d
Benedict Fritz <benedictfritz@gmail.com>
2013-01-06 20:11:41 -0600

    add sloppy throwing force.

--- Assets/Scripts/BallHolder.cs
+++ Assets/Scripts/BallHolder.cs
@@ -5,20 +5,45 @@ public class BallHolder : MonoBehaviour {
 	
 	public Transform ballHolderObject;
 	public string grabKey;
+	public Player player;
+	[HideInInspector] public float throwSpeed = 10000000;
 	[HideInInspector] public bool holdingSomething;
 	[HideInInspector] public Transform holdingObject;
 	
+	[HideInInspector] private bool throwing = false;
+	[HideInInspector] private int throwCount = 4;
+	[HideInInspector] private int throwCounter = 0;
+	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton(grabKey)) {
-			if (holdingSomething) {
-				holdingObject.position = ballHolderObject.position;
-				holdingObject.rigidbody.velocity = Vector3.zero;
-			}
+		if (Input.GetButton(grabKey) && holdingSomething) {
+			holdingObject.position = ballHolderObject.position;
+			holdingObject.rigidbody.velocity = Vector3.zero;
+		}
+		else if (Input.GetButtonUp(grabKey) && holdingSomething) {
+			throwing = true;
+			holdingSomething = false;
 		}
 		else {
 			holdingSomething = false;
-			holdingObject = null;
+		}
+	}
+	
+	void FixedUpdate() {
+		if (throwing) {
+			if (player.name.Equals("player1")) {
+				holdingObject.rigidbody.AddRelativeForce(transform.right * throwSpeed);
+			}
+			else if (player.name.Equals("player2")) {
+				holdingObject.rigidbody.AddRelativeForce(transform.right * -1 * throwSpeed);
+			}
+			
+			throwCounter++;
+			if (throwCounter > throwCount) {
+				throwCounter = 0;
+				holdingObject = null;
+				throwing = false;
+			}
 		}
 	}
 	
